b78db8fb50f76821f9a7245275cb2665529fd8de
Thomas Endresen <thomasjohanendresen@gmail.com>
2018-04-22 12:11:22 +0200

    Fikset problemer med Google map

--- Script/map.js
+++ Script/map.js
@@ -1,7 +1,7 @@
-var map;
+var gmap;
 function initMap() {
     var url = 'https://api.openchargemap.io/v2/poi/?output=json&countrycode=NO&maxresults=*';
-    var map = new google.maps.Map(document.getElementById('map'), {
+    var gmap = new google.maps.Map(document.getElementById('map'), {
         zoom: 4,
         center: new google.maps.LatLng(65.162607, 13.070771),
         mapTypeId: 'terrain'

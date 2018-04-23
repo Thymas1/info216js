7a53a2b660e2263d841f8f4973ea74010c85fa68
Thomas Endresen <thomasjohanendresen@gmail.com>
2018-04-23 12:32:35 +0200

    Satt opp Query motor

--- Script/gmap.js
+++ Script/gmap.js
@@ -3,16 +3,17 @@ function initMap() {
     var url = 'https://api.openchargemap.io/v2/poi/?output=json&countrycode=NO&maxresults=*';
     xhr.open('GET', url, true);
     xhr.onload = function () {
+        xhr.Data = JSON.parse(this.response);
         if (this.status == 200) {
-            var jsonData = JSON.parse(xhr.responseText);
+            xhr.Data.forEach(poi => {
             var map;
             map = new google.maps.Map(document.getElementById('map'), {
-                center: {lat: 62.491507, lng: 9.945628},
-                zoom: 4.5
+                center: {lat: 60.391011, lng: 5.325950},
+                zoom: 4
             });
-            for (var i = 0; i < jsonData.poi.AddressInfo.length; i++) {
-                var data = jsonData.poi.AddressInfo[i],
-                    latLng = new google.maps.LatLng(data.Latitude, data.Longitude);
+            for (var i = 0; i < poi.AddressInfo; i++) {
+                var data = xhr.Data.poi[i],
+                    latLng = new google.maps.LatLng(data.AddressInfo.latitude, data.AddressInfo.longitude);
 
                 //Creating a marker and putting it on the map
                 var marker = new google.maps.Marker({
@@ -21,6 +22,7 @@ function initMap() {
 
                 });
             }
+        })
         }
     }
     xhr.send();

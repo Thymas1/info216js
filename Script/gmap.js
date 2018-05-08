7918503501ba3ed5157bfbfac2a46dd8b19253b1
Thomas Endresen <thomasjohanendresen@gmail.com>
2018-05-08 11:19:28 +0200

    fungerende Geolokasjon

--- Script/gmap.js
+++ Script/gmap.js
@@ -1,5 +1,4 @@
 
-
 function initMap() {
     var xhr = new XMLHttpRequest();
     var url = 'https://api.openchargemap.io/v2/poi/?output=json&countrycode=NO&maxresults=50';
@@ -29,9 +28,36 @@ function initMap() {
                 'Addresse: ' + ' ' + poi.AddressInfo.AddressLine1 + '<br>' + '<a href="vislad.html?id=" + poi.ID> Se mer informasjon</a>'
                 //OM vi ikke får til å lage spesifikke sider generert på bakgrunn av poi.ID legger vi bare inn all nødvendig informasjon her.
             });
+            var infoWind = new google.maps.InfoWindow;
+
             google.maps.event.addListener(marker, 'click', function () {
                 infowindow.open(map, marker);
             });
+                if (navigator.geolocation) {
+                    navigator.geolocation.getCurrentPosition(function (position) {
+                        var pos = {
+                            lat: position.coords.latitude,
+                            lng: position.coords.longitude
+                        };
+                        infoWind.setPosition(pos);
+                        infoWind.setContent('Her er du');
+                        infoWind.open(map);
+                        map.setCenter(pos);
+                    }, function () {
+                        handleLocationError(true, infoWind, map.getCenter());
+                    });
+
+                } else {
+                    handleLocationError(false, infoWind, map.getCenter());
+                }
+                function handleLocationError(browserHasGeolocation, infoWind, pos) {
+                    infoWind.setPosition(pos);
+                    infoWind.setContent(browserHasGeolocation ?
+                        'Error: The Geolocation service failed.' :
+                        'Error: Your browser doesn\'t support geolocation.');
+                    infoWind.open(map);
+                }
+
         })
         }
     }

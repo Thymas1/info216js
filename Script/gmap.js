a4d2b28f21ae8498fcedf20f315839f4c6e9f251
Thomas Endresen <thomasjohanendresen@gmail.com>
2018-05-11 12:29:19 +0200

    Begynt å se på Sparql

--- Script/gmap.js
+++ Script/gmap.js
@@ -30,6 +30,7 @@ function initMap() {
             });
             var infoWind = new google.maps.InfoWindow;
 
+
             google.maps.event.addListener(marker, 'click', function () {
                 infowindow.open(map, marker);
             });
@@ -63,30 +64,9 @@ function initMap() {
         }
     }
     xhr.send();
+
 }
 
 initMap()
 
-function rad(x) { return x*Math.PI/180;}
-function find_closest_marker ( event ) {
-    var lat = event.latLng.lat();
-    var lng = event.latLng.lng();
-    var R = 6371; //Jordens radius i km
-    var distances = [];
-    var closest = -1;
-    for( i=0;i<map.markers.length; i++) {
-        var mlat = map.markers[i].position.lat();
-        var mlng = map.markers[i].position.lng();
-        var dLat = rad(mlat - lat);
-        var dLong = rad(mlng - lng);
-        var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
-            Math.cos(rad(lat)) * Math.cos(rad(lat)) * Math.sin(dLong/2) * Math.sin(dLong/2);
-        var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-        var d = R * c;
-        distances[i] = d;
-        if ( closest == -1 || d < distances[closest] ) {
-            closest = i;
-        }
-    }
-    alert(maps.markers[closest].title);
-}
+

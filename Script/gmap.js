05350d6cec377fd29336fe79a65a98fc42140e61
wubwub <thomas.r.johansen@hotmail.com>
2018-05-22 11:18:28 +0200

    gmap kommentering og rydding

--- Script/gmap.js
+++ Script/gmap.js
@@ -1,12 +1,11 @@
-
-
 function initMap() {
+    //XHR for innhenting av koordinater.
     var xhr = new XMLHttpRequest();
     var url = 'https://api.openchargemap.io/v2/poi/?output=json&countrycode=NO&maxresults=100';
     xhr.open('GET', url, true);
     xhr.onload = function () {
         xhr.Data = JSON.parse(this.response);
-
+        //Lager kartet, og sentrerer kartet over Norge.
         var map = new google.maps.Map(document.getElementById('map'), {
             center: {
                 lat: 60.391011,
@@ -14,24 +13,25 @@ function initMap() {
             },
             zoom: 6
         });
-        if (this.status == 200) {
+        if (this.status === 200) {
+            //Henter ut latitude og longitude fra API og plasserer markers.
             xhr.Data.forEach(poi => {
                 var latLng = new google.maps.LatLng(poi.AddressInfo.Latitude, poi.AddressInfo.Longitude);
-            //Creating a marker and putting it on the map
             var marker = new google.maps.Marker({
                 position: latLng,
                 map: map,
 
             });
-
+            //Lager et infovindu til marker som kommer opp når det klikkes på.
             var infowindow = new google.maps.InfoWindow({
                 content: 'Navn:' + ' ' + poi.AddressInfo.Title + '<br>' +
-                'Addresse: ' + ' ' + poi.AddressInfo.AddressLine1 + '<br>' + '<a href="vislad.html?id=" + poi.ID> Se mer informasjon</a>'
-                //OM vi ikke får til å lage spesifikke sider generert på bakgrunn av poi.ID legger vi bare inn all nødvendig informasjon her.
+                'Addresse: ' + ' ' + poi.AddressInfo.AddressLine1 + '<br>' + poi.ID> Se mer informasjon</a>'
+
             });
+
+
+
             var infoWind = new google.maps.InfoWindow;
-
-
             google.maps.event.addListener(marker, 'click', function () {
                 infowindow.open(map, marker);
             });
